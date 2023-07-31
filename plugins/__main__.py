17c01a4ef88ee04b4d3f99537fb1f697d9a649cf
Veez-Mega <137595410+Veez-Mega@users.noreply.github.com>
2023-07-31 16:09:11 +0530

    Update __main__.py

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -3,10 +3,10 @@ from config.config import API_ID, API_HASH, BOT_TOKEN
 
 downloader = Client(
     ":downloader:",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins.modules") 
+    api_id==API_ID,
+    api_hash==API_HASH,
+    bot_token==BOT_TOKEN,
+    plugins==dict(root="plugins.modules") 
 )
 
 print("Bot started!")
