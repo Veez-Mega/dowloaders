9016c78786c198e3fa8b3efe5aa2d9da25e45f7a
Veez-Mega <137595410+Veez-Mega@users.noreply.github.com>
2023-07-31 16:10:21 +0530

    Update __main__.py

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -3,8 +3,8 @@ from config.config import API_ID, API_HASH, BOT_TOKEN
 
 downloader = Client(
     ":downloader:",
-    api_id==API_ID,
-    api_hash==API_HASH,
+    API_ID,
+    API_HASH,
     bot_token==BOT_TOKEN,
     plugins==dict(root="plugins.modules") 
 )
