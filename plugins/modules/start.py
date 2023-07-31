b3d8af618e4002aeccd7dcdd230f91e43caa592f
Veez-Mega <137595410+Veez-Mega@users.noreply.github.com>
2023-07-31 16:25:58 +0530

    Update start.py

--- plugins/modules/start.py
+++ plugins/modules/start.py
@@ -15,6 +15,6 @@ start_keyboard = InlineKeyboardMarkup( [[
 async def start(client,message):
     await message.reply_photo(
         photo=f"https://graph.org/file/a9541f28afea293440abf.png",
-        caption=f"Hello {}\n\nIam a veez downloader bot i can download all things here you can download any one by simple for more click /help and see in belwo help button.\n\nDeveloped By: @my_name_is_nobitha\nManaged By: @VeezSupportGroup & @levinachannel.",
+        caption=f"Hello {message.from_user.mention()}\n\nIam a veez downloader bot i can download all things here you can download any one by simple for more click /help and see in belwo help button.\n\nDeveloped By: @my_name_is_nobitha\nManaged By: @VeezSupportGroup & @levinachannel.",
         reply_markup=start_keyboard
     )
