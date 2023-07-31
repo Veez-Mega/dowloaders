b7a78ac0bc367ce895b365b6e612a239ae502e3f
Veez-Mega <137595410+Veez-Mega@users.noreply.github.com>
2023-07-31 16:11:30 +0530

    Update __main__.py

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -2,11 +2,10 @@ from pyrogram import Client, filters, idle
 from config.config import API_ID, API_HASH, BOT_TOKEN
 
 downloader = Client(
-    ":downloader:",
     API_ID,
     API_HASH,
-    bot_token==BOT_TOKEN,
-    plugins==dict(root="plugins.modules") 
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins.modules") 
 )
 
 print("Bot started!")
