2088c7f00e24bc424d8f8db54138d98125c995c8
Veez-Mega <137595410+Veez-Mega@users.noreply.github.com>
2023-08-13 12:26:17 +0530

    Update igdown.py

--- plugins/modules/igdown.py
+++ plugins/modules/igdown.py
@@ -6,7 +6,8 @@ import requests
 
 @Client.on_message(filters.command("igdown") & filters.regex(r'https?://(?:www\.)?instagram\.com/p/[-_a-zA-Z0-9]+/?'))
 def download(client, message):
-    instagram_post = message.text.split(" ", 1)[1] 
+    print("Received igdown command")
+    instagram_post = message.text.split(" ", 1)[1]
 
     changing_url = instagram_post.split("/")
     url_code = changing_url[4]
@@ -15,21 +16,26 @@ def download(client, message):
     try:
         visit = requests.get(url).json()
         checking_video = visit['graphql']['shortcode_media']['is_video']
-    except:
+    except Exception as e:
+        print("Error:", e)
         client.send_message(chat_id=message.chat.id, text="Send Me Only Public Instagram Posts ⚡️")
         return
         
     if checking_video:
         try:
             video_url = visit['graphql']['shortcode_media']['video_url']
-            client.send_chat_action(chat_id=message.chat.id, action="typing...")
+            print("Downloading video:", video_url)
+            client.send_chat_action(chat_id=message.chat.id, action="upload_video")
             client.send_video(chat_id=message.chat.id, video=video_url)
-        except:
+        except Exception as e:
+            print("Error downloading video:", e)
             pass
     else:
         try:
             post_url = visit['graphql']['shortcode_media']['display_url']
-            client.send_chat_action(chat_id=message.chat.id, action="typing...")
+            print("Downloading photo:", post_url)
+            client.send_chat_action(chat_id=message.chat.id, action="upload_photo")
             client.send_photo(chat_id=message.chat.id, photo=post_url)
-        except:
+        except Exception as e:
+            print("Error downloading photo:", e)
             pass
